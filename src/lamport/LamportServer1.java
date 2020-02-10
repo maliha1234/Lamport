d046f237d07eda76a3acf3f42c021e73330efa1f
Maliha Sarwat <malihasarwat@cometnet-10-21-78-145.utdallas.edu>
2020-02-10 15:01:13 -0600

    all folder commited

---
+++ src/lamport/LamportServer1.java
@@ -0,0 +1,256 @@
+package lamport;
+
+import java.io.*; 
+import java.text.*; 
+import java.util.*; 
+import java.net.*; 
+  
+// Server class 
+public class LamportServer1
+{ 
+    public static void main(String[] args) throws IOException  
+    { 
+        // server is listening on port 5056 
+        ServerSocket ss = new ServerSocket(5057); 
+          
+        // running infinite loop for getting 
+        // client request 
+        while (true)  
+        { 
+            Socket s = null; 
+              
+            try 
+            { 
+                // socket object to receive incoming client requests 
+                s = ss.accept(); 
+               
+                /*****/
+                
+                String received = new DataInputStream(s.getInputStream()).readUTF(); 
+                  
+                if(received.contains("Client")) {
+                    
+                System.out.println("A new client is connected : " + s); 
+                  
+                // obtaining input and out streams 
+                DataInputStream dis = new DataInputStream(s.getInputStream()); 
+                DataOutputStream dos = new DataOutputStream(s.getOutputStream()); 
+                  
+                System.out.println("Assigning new thread for this client"); 
+                    
+                /******/
+                
+                // create a new thread object 
+                Thread t = new ClientHandler(s, dis, dos); 
+  
+                // Invoking the start() method 
+                t.start(); 
+            }
+                  
+            } 
+            catch (Exception e){ 
+                s.close(); 
+                e.printStackTrace(); 
+            } 
+        } 
+    } 
+} 
+  
+// ClientHandler class 
+class ClientHandler extends Thread  
+{ 
+    DateFormat fordate = new SimpleDateFormat("yyyy/MM/dd"); 
+    DateFormat fortime = new SimpleDateFormat("hh:mm:ss"); 
+    final DataInputStream dis; 
+    final DataOutputStream dos; 
+    final Socket s; 
+    String name;
+    String message;
+      
+  
+    // Constructor 
+    public ClientHandler(Socket s, DataInputStream dis, DataOutputStream dos)  
+    { 
+        this.s = s; 
+        this.dis = dis; 
+        this.dos = dos; 
+    } 
+  
+    @Override
+    public void run()  
+    { 
+        String received; 
+        String toreturn; 
+        
+        /****/
+         try 
+            { 
+                 // getting localhost ip 
+                InetAddress ip = InetAddress.getByName("localhost"); 
+                // establish the connection with server port 5056 
+                Socket s1 = new Socket(ip, 5058); 
+        // obtaining input and out streams
+                
+                DataInputStream dis1 = new DataInputStream(s1.getInputStream()); 
+                DataOutputStream dos1 = new DataOutputStream(s1.getOutputStream()); 
+                
+                 // create a new thread object 
+                Thread sThread = new ServerHandler(s1, dis1, dos1); 
+  
+                // Invoking the start() method 
+                sThread.start(); 
+                
+            }
+         catch (Exception e){ 
+                e.printStackTrace(); 
+            } 
+        /*****/
+        
+        while (true)  
+        { 
+            try { 
+  
+                // Ask user what he wants 
+                dos.writeUTF("What do you want?[Date | Time]..\n"+ 
+                            "Type Exit to terminate connection."); 
+                  
+                // receive the answer from client 
+                received = dis.readUTF(); 
+                  
+                if(received.equals("Exit")) 
+                {  
+                    System.out.println("Client " + this.s + " sends exit..."); 
+                    System.out.println("Closing this connection."); 
+                    this.s.close(); 
+                    System.out.println("Connection closed"); 
+                    break; 
+                } 
+                  
+                // creating Date object 
+                Date date = new Date(); 
+                  
+                // write on output stream based on the 
+                // answer from the client 
+                switch (received) { 
+                  
+                    case "Date" : 
+                        toreturn = fordate.format(date); 
+                        dos.writeUTF(toreturn); 
+                        break; 
+                          
+                    case "Time" : 
+                        toreturn = fortime.format(date); 
+                        dos.writeUTF(toreturn); 
+                        break; 
+                          
+                    default: 
+                        dos.writeUTF("Invalid input"); 
+                        break; 
+                } 
+            } catch (IOException e) { 
+                e.printStackTrace(); 
+            } 
+        } 
+          
+        try
+        { 
+            // closing resources 
+            this.dis.close(); 
+            this.dos.close(); 
+              
+        }catch(IOException e){ 
+            e.printStackTrace(); 
+        } 
+    }
+}
+    
+    
+    // ClientHandler class 
+class ServerHandler extends Thread  
+{ 
+    DateFormat fordate = new SimpleDateFormat("yyyy/MM/dd"); 
+    DateFormat fortime = new SimpleDateFormat("hh:mm:ss"); 
+    final DataInputStream dis; 
+    final DataOutputStream dos; 
+    final Socket s; 
+      
+  
+    // Constructor 
+    public ServerHandler(Socket s, DataInputStream dis, DataOutputStream dos)  
+    { 
+        this.s = s; 
+        this.dis = dis; 
+        this.dos = dos; 
+    } 
+  
+    @Override
+    public void run()  
+    { 
+        String received; 
+        String toreturn; 
+        try { 
+        dos.writeUTF("Server"); 
+        //dos.writeUTF("sending connection request to another server"); 
+        }
+        catch(IOException e){ 
+            e.printStackTrace(); 
+        } 
+        while (true)  
+        { 
+            try { 
+  
+                // Ask user what he wants 
+                dos.writeUTF("sending to server everytime inside run"); 
+                  
+                // receive the answer from client 
+                received = dis.readUTF(); 
+                System.out.println(received); 
+                dos.writeUTF("sending from 1 to server 2"); 
+                  
+                if(received.equals("Exit")) 
+                {  
+                    System.out.println("Client " + this.s + " sends exit..."); 
+                    System.out.println("Closing this connection."); 
+                    this.s.close(); 
+                    System.out.println("Connection closed"); 
+                    break; 
+                } 
+                  
+                // creating Date object 
+                Date date = new Date(); 
+                  
+                // write on output stream based on the 
+                // answer from the client 
+                switch (received) { 
+                  
+                    case "Date" : 
+                        toreturn = fordate.format(date); 
+                        dos.writeUTF(toreturn); 
+                        break; 
+                          
+                    case "Time" : 
+                        toreturn = fortime.format(date); 
+                        dos.writeUTF(toreturn); 
+                        break; 
+                          
+                    default: 
+                        dos.writeUTF("Invalid input"); 
+                        break; 
+                } 
+            } catch (IOException e) { 
+                e.printStackTrace(); 
+            } 
+        } 
+          
+        try
+        { 
+            // closing resources 
+            this.dis.close(); 
+            this.dos.close(); 
+              
+        }catch(IOException e){ 
+            e.printStackTrace(); 
+        } 
+    }
+}
+ 
---
+++ src/lamport/LamportServer2.java
@@ -0,0 +1,238 @@
+package lamport;
+
+import java.io.*; 
+import java.text.*; 
+import java.util.*; 
+import java.net.*; 
+  
+// Server class 
+public class LamportServer2
+{ 
+    public static void main(String[] args) throws IOException  
+    { 
+        // server is listening on port 5056 
+        ServerSocket ss = new ServerSocket(5058); 
+          
+        // running infinite loop for getting 
+        // client request 
+        while (true)  
+        { 
+            Socket s = null; 
+              
+            try 
+            { 
+                // socket object to receive incoming client requests 
+                s = ss.accept(); 
+               
+                /*****/
+                
+                String received = new DataInputStream(s.getInputStream()).readUTF(); 
+                System.out.println(received); 
+                  
+                if(received.contains("Client")) {
+                    
+                System.out.println("A new client is connected : " + s); 
+                  
+                // obtaining input and out streams 
+                DataInputStream dis = new DataInputStream(s.getInputStream()); 
+                DataOutputStream dos = new DataOutputStream(s.getOutputStream()); 
+                  
+                System.out.println("Assigning new thread for this client"); 
+                    
+                /******/
+                
+                // create a new thread object 
+                Thread t = new ClientHandler(s, dis, dos); 
+  
+                // Invoking the start() method 
+                t.start(); 
+            }
+                else if(received.contains("Server")) { 
+                    // obtaining input and out streams 
+                DataInputStream dis = new DataInputStream(s.getInputStream()); 
+                DataOutputStream dos = new DataOutputStream(s.getOutputStream()); 
+                  
+                System.out.println("Assigning new thread for this server"); 
+                    
+                /******/
+                
+                // create a new thread object 
+                Thread serverThread = new ServerHandler2(s, dis, dos); 
+                serverThread.start();
+                }
+                
+                  
+            } 
+            catch (Exception e){ 
+                s.close(); 
+                e.printStackTrace(); 
+            } 
+        } 
+    } 
+} 
+  
+// ClientHandler class 
+class ClientHandler2 extends Thread  
+{ 
+    DateFormat fordate = new SimpleDateFormat("yyyy/MM/dd"); 
+    DateFormat fortime = new SimpleDateFormat("hh:mm:ss"); 
+    final DataInputStream dis; 
+    final DataOutputStream dos; 
+    final Socket s; 
+    String name;
+    String message;
+      
+  
+    // Constructor 
+    public ClientHandler2(Socket s, DataInputStream dis, DataOutputStream dos)  
+    { 
+        this.s = s; 
+        this.dis = dis; 
+        this.dos = dos; 
+    } 
+  
+    @Override
+    public void run()  
+    { 
+        String received; 
+        String toreturn; 
+        while (true)  
+        { 
+            try { 
+  
+                // Ask user what he wants 
+                dos.writeUTF("What do you want?[Date | Time]..\n"+ 
+                            "Type Exit to terminate connection."); 
+                  
+                // receive the answer from client 
+                received = dis.readUTF(); 
+                  
+                if(received.equals("Exit")) 
+                {  
+                    System.out.println("Client " + this.s + " sends exit..."); 
+                    System.out.println("Closing this connection."); 
+                    this.s.close(); 
+                    System.out.println("Connection closed"); 
+                    break; 
+                } 
+                  
+                // creating Date object 
+                Date date = new Date(); 
+                  
+                // write on output stream based on the 
+                // answer from the client 
+                switch (received) { 
+                  
+                    case "Date" : 
+                        toreturn = fordate.format(date); 
+                        dos.writeUTF(toreturn); 
+                        break; 
+                          
+                    case "Time" : 
+                        toreturn = fortime.format(date); 
+                        dos.writeUTF(toreturn); 
+                        break; 
+                          
+                    default: 
+                        dos.writeUTF("Invalid input"); 
+                        break; 
+                } 
+            } catch (IOException e) { 
+                e.printStackTrace(); 
+            } 
+        } 
+          
+        try
+        { 
+            // closing resources 
+            this.dis.close(); 
+            this.dos.close(); 
+              
+        }catch(IOException e){ 
+            e.printStackTrace(); 
+        } 
+    }
+}
+    
+    
+    // ClientHandler class 
+class ServerHandler2 extends Thread  
+{ 
+    DateFormat fordate = new SimpleDateFormat("yyyy/MM/dd"); 
+    DateFormat fortime = new SimpleDateFormat("hh:mm:ss"); 
+    final DataInputStream dis; 
+    final DataOutputStream dos; 
+    final Socket s; 
+      
+  
+    // Constructor 
+    public ServerHandler2(Socket s, DataInputStream dis, DataOutputStream dos)  
+    { 
+        this.s = s; 
+        this.dis = dis; 
+        this.dos = dos; 
+    } 
+  
+    @Override
+    public void run()  
+    { 
+        String received; 
+        String toreturn; 
+        while (true)  
+        { 
+            try { 
+  
+                // Ask user what he wants 
+                dos.writeUTF("what update from you server 1 to me server 2"); 
+                  
+                // receive the answer from client 
+                received = dis.readUTF(); 
+                System.out.println(received); 
+                  
+                if(received.equals("Exit")) 
+                {  
+                    System.out.println("Client " + this.s + " sends exit..."); 
+                    System.out.println("Closing this connection."); 
+                    this.s.close(); 
+                    System.out.println("Connection closed"); 
+                    break; 
+                } 
+                  
+                // creating Date object 
+                Date date = new Date(); 
+                  
+                // write on output stream based on the 
+                // answer from the client 
+                switch (received) { 
+                  
+                    case "Write" : 
+                        toreturn = fordate.format(date); 
+                        dos.writeUTF(toreturn); 
+                        break; 
+                          
+                    case "Read" : 
+                        toreturn = fortime.format(date); 
+                        dos.writeUTF(toreturn); 
+                        break; 
+                          
+                    default: 
+                        dos.writeUTF("Invalid input"); 
+                        break; 
+                } 
+            } catch (IOException e) { 
+                e.printStackTrace(); 
+            } 
+        } 
+          
+        try
+        { 
+            // closing resources 
+            this.dis.close(); 
+            this.dos.close(); 
+              
+        }catch(IOException e){ 
+            e.printStackTrace(); 
+        } 
+    }
+}
+ 
