e77814e27bd31859937ba0cbb1037adc20b31ead
Maliha Sarwat <malihasarwat@cometnet-10-21-11-255.utdallas.edu>
2020-02-14 12:00:03 -0600

    will get back to this version. statble may be

--- src/lamport/NewLamportServer1.java
+++ src/lamport/NewLamportServer1.java
@@ -65,17 +65,17 @@ public class NewLamportServer1 {
                     DataInputStream dis2 = new DataInputStream(s2.getInputStream());
                     DataOutputStream dos2 = new DataOutputStream(s2.getOutputStream());
 
+                    long timeStamp = System.currentTimeMillis();
                     // create a new thread object 
                     Thread t = new ClientHandler4(s, dis, dos);
 
-                    Thread sT = new ServerSocketHandler4(s1, dis1, dos1, s2, dis2, dos2);
+                    Thread sT = new ServerSocketHandler4(s1, dis1, dos1, s2, dis2, dos2, timeStamp, received);
 
-                    // String id = 
                     // Invoking the start() method 
                     t.start();
                     sT.start();
 
-                    QueueClass4 queueClass = new QueueClass4(System.currentTimeMillis(), 1, received, (ClientHandler4) t, (ServerSocketHandler4) sT);
+                    QueueClass4 queueClass = new QueueClass4(timeStamp, 1, received, (ClientHandler4) t, (ServerSocketHandler4) sT);
 
                     qList.add(queueClass);
 
@@ -242,25 +242,29 @@ class ServerSocketHandler4 extends Thread {
     final DataInputStream dis2;
     final DataOutputStream dos2;
     final Socket s2;
+    long timeStamp;
+    String receivedMessage;
 
     public int ackFromOthers = 0;
 
     // Constructor 
-    public ServerSocketHandler4(Socket s1, DataInputStream dis1, DataOutputStream dos1, Socket s2, DataInputStream dis2, DataOutputStream dos2) {
+    public ServerSocketHandler4(Socket s1, DataInputStream dis1, DataOutputStream dos1, Socket s2, DataInputStream dis2, DataOutputStream dos2, long time, String message) {
         this.s1 = s1;
         this.dis1 = dis1;
         this.dos1 = dos1;
         this.s2 = s2;
         this.dis2 = dis2;
         this.dos2 = dos2;
+        this.timeStamp = time;
+        this.receivedMessage = message;
 
-        /* try {
-                // Ask user what he wants 
-                 dos.writeUTF("connectedwithserver");
+        try {
+            // Ask user what he wants 
+            dos1.writeUTF("connectedwithserver," + timeStamp + "," + receivedMessage);
 
-            } catch (IOException ex) {
-                Logger.getLogger(ClientHandler4.class.getName()).log(Level.SEVERE, null, ex);
-            }*/
+        } catch (IOException ex) {
+            Logger.getLogger(ClientHandler4.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
@@ -285,6 +289,9 @@ class ServerSocketHandler4 extends Thread {
 
                     case "ackreceived":
                         dos1.writeUTF("okyoureceivedack");
+                        break;
+                    case "processedinserver2":
+                        dos1.writeUTF("processedinserver2gotit");
                         break;
 
                     case "ack":
--- src/lamport/NewLamportServer2.java
+++ src/lamport/NewLamportServer2.java
@@ -5,148 +5,219 @@
  */
 package lamport;
 
-import java.io.*; 
-import java.text.*; 
-import java.util.*; 
-import java.net.*; 
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.net.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-  
+
 // Server class 
-public class NewLamportServer2  
-{ 
-    public static void main(String[] args) throws IOException  
-    { 
+public class NewLamportServer2 {
+
+    public static List<QueueClass5> qList = new ArrayList<>();
+    public static int flag = 0;
+
+    public static void main(String[] args) throws IOException {
         // server is listening on port 5056 
-        ServerSocket ss = new ServerSocket(5051); 
-          
+        ServerSocket ss = new ServerSocket(5051);
+         if (flag == 0) {
+            startQueueThread();
+            flag = 1;
+        }
+
         // running infinite loop for getting 
         // client request 
-        while (true)  
-        { 
-            Socket s = null; 
-              
-            try 
-            { 
+        while (true) {
+            Socket s = null;
+
+            try {
                 // socket object to receive incoming client requests 
-                s = ss.accept(); 
-                  
-                System.out.println("A new client is connected : " + s); 
-                  
-                // obtaining input and out streams 
-                DataInputStream dis = new DataInputStream(s.getInputStream()); 
-                DataOutputStream dos = new DataOutputStream(s.getOutputStream()); 
-                  
-                System.out.println("Assigning new thread for this client"); 
-  
-                // create a new thread object 
-                Thread t = new ServerHandler5(s, dis, dos); 
-  
-                // Invoking the start() method 
-                t.start(); 
-                  
-            } 
-            catch (Exception e){ 
-                s.close(); 
-                e.printStackTrace(); 
-            } 
-        } 
-    } 
-} 
-  
+                s = ss.accept();
+
+                System.out.println("A new client is connected : " + s);
+                String received = new DataInputStream(s.getInputStream()).readUTF();
+                System.out.println("in main" + received);
+
+                if (received.contains("connectedwithserver")) {
+
+                    String[] arrOfStr = received.split(",", 5);
+                    System.out.println(arrOfStr[0]);
+                    long timeStamp = Long.parseLong(arrOfStr[1]);
+                    String message = arrOfStr[2];
+                    // obtaining input and out streams 
+                    DataInputStream dis = new DataInputStream(s.getInputStream());
+                    DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+
+                    System.out.println("Assigning new thread for this client");
+
+                    // create a new thread object 
+                    Thread t = new ServerHandler5(s, dis, dos, timeStamp, message);
+                    QueueClass5 queueClass = new QueueClass5(timeStamp, 1, message, (ServerHandler5) t);
+
+                    qList.add(queueClass);
+
+                    // Invoking the start() method 
+                    t.start();
+                }
+
+            } catch (Exception e) {
+                s.close();
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    public static void startQueueThread() throws IOException {
+        Timer t = new Timer();
+        System.out.println("Hello World Timer ini");
+        t.schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+                try {
+                    System.out.println("Hello World" + qList);
+                    if (!qList.isEmpty()) {
+                        QueueClass5 qClass = qList.get(0);
+                        String task = qList.get(0).task;
+                        System.out.println("Hello World task in server 2" + task + "\n");
+                        if (qClass.clientClassHandler != null) {
+
+                            qClass.clientClassHandler.dos.writeUTF("acktoclient");
+                        }
+                        if (qClass.serverHandler != null) {
+                            qClass.serverHandler.dos.writeUTF("processedinserver2");
+                           // qClass.serverHandler.dos2.writeUTF("ackto3," + task);
+
+                        }
+
+                        for (QueueClass5 qq : qList) {
+                            System.out.println("Hello World" + qq.timestamp + "\n");
+                        }
+                    }
+
+                } catch (IOException ex) {
+                    Logger.getLogger(LamportServer1.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+            }
+        }, 0, 5000);
+
+    }
+}
+
 // ClientHandler class 
-class ServerHandler5 extends Thread  
-{ 
-    DateFormat fordate = new SimpleDateFormat("yyyy/MM/dd"); 
-    DateFormat fortime = new SimpleDateFormat("hh:mm:ss"); 
-    final DataInputStream dis; 
-    final DataOutputStream dos; 
-    final Socket s; 
-      
-  
+class ServerHandler5 extends Thread {
+
+    DateFormat fordate = new SimpleDateFormat("yyyy/MM/dd");
+    DateFormat fortime = new SimpleDateFormat("hh:mm:ss");
+    final DataInputStream dis;
+    final DataOutputStream dos;
+    final Socket s;
+
+    long timeStamp;
+    String receivedMessage;
+
     // Constructor 
-    public ServerHandler5(Socket s, DataInputStream dis, DataOutputStream dos)  
-    { 
-        this.s = s; 
-        this.dis = dis; 
-        this.dos = dos; 
-        
-           try {
+    public ServerHandler5(Socket s, DataInputStream dis, DataOutputStream dos, long time, String message) {
+        this.s = s;
+        this.dis = dis;
+        this.dos = dos;
+        this.timeStamp = time;
+        this.receivedMessage = message;
+
+        try {
             dos.writeUTF("connected");
-            
+
         } catch (IOException ex) {
             Logger.getLogger(ClientHandler4.class.getName()).log(Level.SEVERE, null, ex);
         }
-    } 
-  
+    }
+
     @Override
-    public void run()  
-    { 
-        String received; 
-        String toreturn; 
-        
-        while (true)  
-        { 
-            try { 
-  
+    public void run() {
+        String received;
+        String toreturn;
+
+        while (true) {
+            try {
+
                 // Ask user what he wants 
-              //  dos.writeUTF("server 2"); 
-                  
+                //  dos.writeUTF("server 2"); 
                 // receive the answer from client 
                 received = dis.readUTF();
-                System.out.println(received); 
-                  
-                if(received.equals("Exit")) 
-                {  
-                    System.out.println("Client " + this.s + " sends exit..."); 
-                    System.out.println("Closing this connection."); 
-                    this.s.close(); 
-                    System.out.println("Connection closed"); 
-                    break; 
-                } 
-                  
+                System.out.println(received);
+
+                if (received.equals("Exit")) {
+                    System.out.println("Client " + this.s + " sends exit...");
+                    System.out.println("Closing this connection.");
+                    this.s.close();
+                    System.out.println("Connection closed");
+                    break;
+                }
+
                 // creating Date object 
-                Date date = new Date(); 
-                  
+                Date date = new Date();
+
                 // write on output stream based on the 
                 // answer from the client 
-                String[] arrOfStr = received.split(",", 5); 
-                System.out.println(arrOfStr[0]); 
-                switch (arrOfStr[0]) { 
-                  
-                    case "ok" : 
-                         
-                        dos.writeUTF("okfromserver2"); 
-                        break; 
-                    case "ackto2" : 
-                         
-                        dos.writeUTF("ackto2okfromserver2"); 
-                        break; 
-                          
-                    case "okyoureceivedack" : 
-                        toreturn = fortime.format(date); 
-                        dos.writeUTF(toreturn); 
-                        break; 
-                          
-                    default: 
+                String[] arrOfStr = received.split(",", 5);
+                System.out.println(arrOfStr[0]);
+                switch (arrOfStr[0]) {
+
+                    case "ok":
+
+                        dos.writeUTF("okfromserver2");
+                        break;
+                    case "ackto2":
+
+                        dos.writeUTF("ackto2okfromserver2");
+                        break;
+                    case "connectedwithserver":
+
+                        dos.writeUTF("connectedwithserver2Received");
+                        break;
+                    case "okyoureceivedack":
+                        toreturn = fortime.format(date);
+                        dos.writeUTF(toreturn);
+                        break;
+
+                    default:
                         //dos.writeUTF("Invalid input"); 
-                        break; 
-                } 
-            } catch (IOException e) { 
-                e.printStackTrace(); 
-            } 
-        } 
-          
-        try
-        { 
+                        break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
             // closing resources 
-            this.dis.close(); 
-            this.dos.close(); 
-              
-        }catch(IOException e){ 
-            e.printStackTrace(); 
-        } 
-    } 
-    
-    
-} 
+            this.dis.close();
+            this.dos.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class QueueClass5 {
+
+    long timestamp;
+    int serverId;
+    String task;
+    ClientHandler4 clientClassHandler;
+    ServerHandler5 serverHandler;
+
+    // Constructor 
+    public QueueClass5(long timeStamp, int serverId, String task, ServerHandler5 s) {
+        this.serverId = serverId;
+        this.timestamp = timeStamp;
+        this.task = task;
+        // this.clientClassHandler = c;
+        this.serverHandler = s;
+
+    }
+
+}
